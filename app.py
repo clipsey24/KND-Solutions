68c866cd7be780ae3169c4d59f9f72e19be0f10f
clipsey24 <christopher.lipsey@gmail.com>
2023-11-24 14:30:40 +0000

    added data

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, jsonify, render_template
 
 app = Flask(__name__)
 
@@ -21,7 +21,7 @@ JOBS = [
       'id': 3,
       'title': 'Frontend Engineer',
       'location': 'Remote',
-      'salary': 'Rs. 12,00,000'
+      
    },
 
 {
@@ -34,7 +34,12 @@ JOBS = [
 
 @app.route("/")
 def hello_world():
-    return render_template('home.html')
+    return render_template('home.html', jobs=JOBS)
+
+
+@app.route("/api/jobs")
+def list_jobs():
+    return jsonify(JOBS)
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
