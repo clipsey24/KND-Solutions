88f4fcea0a44d1523b4c157bf892dd57c693a516
clipsey24 <christopher.lipsey@gmail.com>
2023-12-15 20:24:31 +0000

    Created services

--- app.py
+++ app.py
@@ -2,9 +2,32 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+SERVICES = [
+  {
+    'id': 1,
+    'service': 'IT Consulting',
+    'Description': 'TODO'
+  },
+  {
+    'id': 2,
+    'service': 'Website Host/Build',
+    'Description': 'TODO'
+  },
+  {
+    'id': 3,
+    'service': 'Computer Repair/Installation',
+    'Description': 'TODO'
+  },
+  {
+    'id': 4,
+    'service': 'Network Installation/Troubleshooting',
+    'Description': 'TODO'
+  }
+]
+
 @app.route("/")
 def KND():
-    return render_template('home.html')
+    return render_template('home.html', service=SERVICES)
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
