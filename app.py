3b21d00604b3fcc5f731ca5f6c6055a6d378632c
clipsey24 <christopher.lipsey@gmail.com>
2023-12-23 03:36:54 +0000

    changes to home and add nav and footer

--- app.py
+++ app.py
@@ -6,22 +6,22 @@ SERVICES = [
   {
     'id': 1,
     'service': 'IT Consulting',
-    'Description': 'TODO'
+    'Description': ' Assess existing IT infrastructures, identify areas for improvement, and offer strategic recommendations to enhance efficiency, security, and overall performance.'
   },
   {
     'id': 2,
     'service': 'Website Host/Build',
-    'Description': 'TODO'
+    'Description': 'Website hosting and building encompass the processes involved in creating and managing a website'
   },
   {
     'id': 3,
     'service': 'Computer Repair/Installation',
-    'Description': 'TODO'
+    'Description': 'Computer repair and installation involve addressing hardware and software issues, as well as setting up new systems to ensure they function optimally.'
   },
   {
     'id': 4,
     'service': 'Network Installation/Troubleshooting',
-    'Description': 'TODO'
+    'Description': 'Network installation and troubleshooting involve setting up and maintaining the infrastructure that allows devices to communicate and share information within an organization or across multiple locations.'
   }
 ]
 
@@ -32,6 +32,10 @@ def KND():
 @app.route("/api/service")
 def list_services():
   return jsonify(SERVICES)
+
+@app.route("/")
+def about():
+    return render_template('about.html')
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
