e5dc483eddee56604f9342cc286c5cd114a20f08
clipsey24 <christopher.lipsey@gmail.com>
2024-01-08 16:26:25 +0000

    Adding pricing

--- app.py
+++ app.py
@@ -6,22 +6,27 @@ SERVICES = [
   {
     'id': 1,
     'service': 'IT Consulting',
-    'Description': ' Assess existing IT infrastructures, identify areas for improvement, and offer strategic recommendations to enhance efficiency, security, and overall performance.'
+    'Description': ' Assess existing IT infrastructures, identify areas for improvement, and offer strategic recommendations to enhance efficiency, security, and overall performance.',
+    'price': '$100 to $300 per hour'
   },
   {
     'id': 2,
     'service': 'Website Host/Build',
-    'Description': 'Website hosting and building encompass the processes involved in creating and managing a website'
+    'Description': 'Website hosting and building encompass the processes involved in creating and managing a website',
+    'price': 'We have several different plans to fit all budgets'
+    
   },
   {
     'id': 3,
     'service': 'Computer Repair/Installation',
-    'Description': 'Computer repair and installation involve addressing hardware and software issues, as well as setting up new systems to ensure they function optimally.'
+    'Description': 'Computer repair and installation involve addressing hardware and software issues, as well as setting up new systems to ensure they function optimally.',
+    'price': '$50 to $150 per hour'
   },
   {
     'id': 4,
     'service': 'Network Installation/Troubleshooting',
-    'Description': 'Network installation and troubleshooting involve setting up and maintaining the infrastructure that allows devices to communicate and share information within an organization or across multiple locations.'
+    'Description': 'Network installation and troubleshooting involve setting up and maintaining the infrastructure that allows devices to communicate and share information within an organization or across multiple locations.',
+    'price': '$75 per hour for small jobs'
   }
 ]
 
